0ece9171ce973967ca6213191849e8fb454f9461
Nadeem Anwar <ibnul.aftab@proton.me>
2025-02-21 21:40:46 +0500

    basic skeleton
    Inititated for expression input

--- calculator.c
+++ calculator.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+int main (char argc, char *argv[]) {
+	// checks for tags
+	if (argv[1][0] == '-') {
+		// check for tag type
+		
+		// tag = expression
+		if (arg[1][1] = 'e') {
+			
+		}
+
+	} else {
+		printf("calc: This command is not recognized.\n");
+	}
+
+	return 0;
+}
