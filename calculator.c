182fdeadd9548520364b502828a8dfc910c3ca27
Nadeem Anwar <ibnul.aftab@proton.me>
2025-02-21 22:20:15 +0500

    added operation struct

--- calculator.c
+++ calculator.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+struct operations {
+	int* operand1;
+	char operation;
+	int* operand2;
+}
+
 int main (char argc, char *argv[]) {
 	// checks for tags
 	if (argv[1][0] == '-') {
 		// check for tag type
 		
 		// tag = expression
-		if (arg[1][1] = 'e') {
+		if (arg[1][1] == 'e') {
 			
 		}
 
