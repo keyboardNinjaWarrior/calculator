f011169ef3dc11a014026e3ec6a1a24bf0431b6e
Nadeem Anwar <ibnul.aftab@proton.me>
2025-03-05 23:58:02 +0500

    still working on the fuction of parsing? don't know if it is going to work... :/

--- expression.c
+++ expression.c
@@ -1,5 +1,7 @@
 #include "lib.h"
 
+long double parse_operand (char *, int);
+
 typedef struct exptree {
 	bool is_end;
 
@@ -18,9 +20,16 @@ typedef struct exptree {
 
 void expression (char *exp) {
 	exptree *tree = malloc (sizeof (exptree));
+	printf("%llf", parse_operand (exp, 0));
 }
 
-long double parse_operand (char *exp) {
-
-
+long double parse_operand (char *exp, int start) {	
+	long double operand = 0;
+	if (exp[start] >= '0' && exp[start] <= '9') {
+		for(;exp[start] >= '9' || exp[start] <= '0'; start++) {
+			operand *= 10;
+			operand += exp[start] - '0';
+		}
+	}
+	return operand;
 }
