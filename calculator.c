cb8e0102467e0f851b04f9eb51b29d06abd39f93
Nadeem Anwar <ibnul.aftab@proton.me>
2025-03-05 23:04:31 +0500

    revert to pre parsing code
    This reverts commit 56021f252c15bfd1a86b6356b032ccba81555a0a.

--- calculator.c
+++ calculator.c
@@ -1,12 +1,11 @@
 #include "header.h"
 
 int main (char argc, char *argv[]) {
-	
-         /* * * * * * * * * * * * * * * *
-	 * '-':	tags intitialized	*
-	 * 'e': expression		*
-	 * * * * * * * * * * * * * * * */
+	// checks for tags
 	if (argv[1][0] == '-') {
+		// check for tag type
+		
+		// tag = expression
 		if (argv[1][1] == 'e') {
 			expression (argv[2]);
 		}
