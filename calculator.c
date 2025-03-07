b57cc5d3fb9fdc7221902c31ba259c1a25843261
Nadeem Anwar <ibnul.aftab@proton.me>
2025-03-07 17:05:00 +0500

    added comments and exit function

--- calculator.c
+++ calculator.c
@@ -2,22 +2,28 @@
 
 char *remove_spaces (char *);
 
+// -	tag init
+// e	expression
+
 int main (char argc, char *argv[]) {
-	// checks for tags
 	if (argv[1][0] == '-') {
-		// check for tag type
-		
-		// tag = expression
 		if (argv[1][1] == 'e') {
 			expression (remove_spaces (argv[2]));
 		}
 
 	} else {
-		printf("calc: This command is not recognized.\n");
+		printf ("calc: this command is not recognized.\n");
+		exit (1);
 	}
 
 	return 0;
 }
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * remove spaces: It takes the string as input, calculates its length and 	*
+ *	 	  removes the space by placing the next non-space character 	*
+ *	 	  at its position and then add \0 on remaining locations.	*
+ * * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 char *remove_spaces (char *exp) {
 	int end = 0, position = 0;
