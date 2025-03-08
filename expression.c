62a91cb13d4bf3e7e9cce06c38d0dce4689cbfe8
Nadeem Anwar <ibnul.aftab@proton.me>
2025-03-08 09:02:57 +0500

    changed the resturn type of parse operand function

--- expression.c
+++ expression.c
@@ -1,6 +1,6 @@
 #include "lib.h"
 
-long double parse_operand (char *, int);
+long double *parse_operand (char *, int);
 
 typedef struct exptree {
 	bool is_end;
@@ -20,16 +20,19 @@ typedef struct exptree {
 
 void expression (char *exp) {
 //	exptree *tree = malloc (sizeof (exptree));
-	printf("%llf", parse_operand (exp, 0));
+	printf("%llf", *parse_operand (exp, 0));
 }
 
-long double parse_operand (char *exp, int start) {	
-	long double operand = 0;
-	if (exp[start] >= '0' && exp[start] <= '9') {
-		for(; exp[start] >= '0' && exp[start] <= '9'; start++) {
-			operand *= 10;
-			operand += exp[start] - '0';
-		}
+long double *parse_operand (char *exp, int start) {	
+	long double *operand;
+	if (!(operand = malloc (sizeof (long double)))) {
+		exit (3);
+	}
+
+	*operand = 0;
+	for(; exp[start] >= '0' && exp[start] <= '9'; start++) {
+		*operand *= 10;
+		*operand += exp[start] - '0';
 	}
 	return operand;
 }
