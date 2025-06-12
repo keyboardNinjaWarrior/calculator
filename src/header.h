83df3e5c50c7581bc55971d26c44edd301ecb6d3
Nadeem Anwar <ibnul.aftab@proton.me>
2025-06-12 20:09:58 +0500

    Added files

---
+++ src/header.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+void parse_number (void);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "header.h"
+
+int main (int argc, char *argv[]) {
+	char *number = argv[1];
+	
+	parse_number ();	
+
+	return 0;
+}
